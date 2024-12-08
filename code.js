745e45984d930b6e79ebeaad03240f8dc5438fc1
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2024-12-07 23:35:04 -0600

    styling changes to page, and update My Work portion at bottom. Also rewored About and Experience pages

--- code.js
+++ code.js
@@ -1,120 +1,239 @@
-// Local Storage Vars
-
-let darkmode = localStorage.getItem("darkmode") ?? 1;
+// Dark Mode handling:
+let darkmode = localStorage.getItem("darkmode") ?? 0; // Default to light mode
 let darkLightModeEl = document.getElementById("lightmode-darkmode");
 
 function darkMode() {
-    darkLightModeEl.innerHTML = "01001100"; 
-    document.documentElement.style.setProperty("filter", "none");
-    document.getElementById("main-page-projects-wrapper").style.setProperty("filter", "none");
+  darkLightModeEl.innerHTML = "01001100";
+  document.documentElement.style.setProperty("filter", "none");
+  document
+    .getElementById("main-page-projects-wrapper")
+    .style.setProperty("filter", "none");
+  darkLightModeEl.setAttribute("title", "Set to light mode.");
 }
 
 function lightMode() {
-    darkLightModeEl.innerHTML = "01000100"; 
-    document.documentElement.style.setProperty("filter", "invert()");
-    document.getElementById("main-page-projects-wrapper").style.setProperty("filter", "invert()");
+  darkLightModeEl.innerHTML = "01000100";
+  document.documentElement.style.setProperty("filter", "invert()");
+  document
+    .getElementById("main-page-projects-wrapper")
+    .style.setProperty("filter", "invert()");
+  darkLightModeEl.setAttribute("title", "Set to dark mode.");
 }
 
 if (parseInt(darkmode) === 1) {
-    lightMode();
-    darkLightModeEl.setAttribute("title", "Set to light mode.");
+  darkMode();
+} else if (parseInt(darkmode) === 0) {
+  lightMode();
 }
-else if (parseInt(darkmode) === 0) {
-    darkMode();
-    darkLightModeEl.setAttribute("title", "Set to dark mode.");
+
+// Tickle Flicker handling:
+const symbols = ["λ", "%", "_", "[", "{"];
+const openingBrackets = ["[", "{"];
+const closingBrackets = ["]", "}"];
+const validOpeningBracketIndices = [0, 1, 2, 3, 7, 8, 9];
+let previousSymbol;
+
+const interval = 13500;
+
+function flickerEffect() {
+  const titleElementChildren =
+    window.screen.width > 605
+      ? document.getElementById("fullscreentitle").children
+      : [
+          ...document.getElementById("smallscreentitle1").children,
+          ...document.getElementById("smallscreentitle2").children,
+        ];
+
+  let symbolIndex, randomSymbol;
+  do {
+    symbolIndex = Math.floor(Math.random() * symbols.length);
+    randomSymbol = randomSymbol = symbols[symbolIndex];
+    console.log(randomSymbol);
+  } while (previousSymbol === randomSymbol);
+
+  previousSymbol = randomSymbol;
+
+  let letterIndex;
+  do {
+    letterIndex = Math.floor(Math.random() * titleElementChildren.length);
+  } while (
+    titleElementChildren[letterIndex].classList.contains("flicker-disabled") ||
+    (openingBrackets.includes(randomSymbol) &&
+      !validOpeningBracketIndices.includes(letterIndex))
+  );
+
+  const firstLetter = titleElementChildren[letterIndex];
+  if (openingBrackets.includes(randomSymbol)) {
+    const closingSymbol =
+      closingBrackets[openingBrackets.findIndex((v) => v === randomSymbol)];
+    let secondLetterIndex;
+    do {
+      secondLetterIndex = Math.floor(
+        Math.random() * titleElementChildren.length
+      );
+    } while (
+      titleElementChildren[secondLetterIndex].classList.contains(
+        "flicker-disabled"
+      ) ||
+      secondLetterIndex <= letterIndex ||
+      secondLetterIndex === letterIndex + 1 ||
+      secondLetterIndex > letterIndex + 3
+    );
+    const secondLetter =
+      titleElementChildren[secondLetterIndex] ??
+      titleElementChildren[titleElementChildren.length - 1];
+    replaceLetter(secondLetter, closingSymbol);
+  }
+
+  replaceLetter(firstLetter, randomSymbol);
 }
+
+function replaceLetter(letter, symbol) {
+  const prevLetter = letter.innerHTML;
+  letter.innerHTML = symbol;
+  letter.classList.add("flicker");
+
+  setTimeout(() => {
+    letter.classList.remove("flicker");
+  }, 200);
+
+  setTimeout(() => {
+    letter.classList.add("flicker");
+    letter.innerHTML = prevLetter;
+    setTimeout(() => {
+      letter.classList.remove("flicker");
+    }, 200);
+  }, 3000);
+}
+
+setInterval(flickerEffect, interval);
 
 // Event Listeners:
-
 document.getElementById("about-link").addEventListener("click", function (e) {
-    handleNavClick(e);
+  handleNavClick(e);
 });
-document.getElementById("about-link").addEventListener("touchstart", function (e) {
+document
+  .getElementById("about-link")
+  .addEventListener("touchstart", function (e) {
     handleNavClick(e);
-});
+  });
 
-document.getElementById("work-link").addEventListener("click", function (e) {
+document
+  .getElementById("experience-link")
+  .addEventListener("click", function (e) {
     handleNavClick(e);
-});
-document.getElementById("work-link").addEventListener("touchstart", function (e) {
+  });
+document
+  .getElementById("experience-link")
+  .addEventListener("touchstart", function (e) {
     handleNavClick(e);
-});
+  });
 
 document.getElementById("contact-link").addEventListener("click", function (e) {
-    handleNavClick(e);
+  handleNavClick(e);
 });
-document.getElementById("contact-link").addEventListener("touchstart", function (e) {
+document
+  .getElementById("contact-link")
+  .addEventListener("touchstart", function (e) {
     handleNavClick(e);
-});
+  });
 
 darkLightModeEl.addEventListener("click", handleDarkMode);
 
-document.getElementById("main-page-contact-list-emailcopy").addEventListener("click", handleCopy)
+document
+  .getElementById("main-page-contact-list-emailcopy")
+  .addEventListener("click", handleCopy);
 
-// Functions To Handle Logic:
-
+// Functions to Handle Logic:
 function handleDarkMode() {
-    if (parseInt(darkmode) === 1) {
-        darkmode = 0;
-        darkMode();
-        localStorage.setItem("darkmode", darkmode);
-    }
-    else if (parseInt(darkmode) === 0) {
-        darkmode = 1;
-        lightMode();
-        localStorage.setItem("darkmode", darkmode);
-    }
+  if (parseInt(darkmode) === 1) {
+    lightMode();
+    darkmode = 0;
+    localStorage.setItem("darkmode", darkmode);
+  } else if (parseInt(darkmode) === 0) {
+    darkMode();
+    darkmode = 1;
+    localStorage.setItem("darkmode", darkmode);
+  }
 }
 
 function handleCopy() {
-    navigator.clipboard.writeText("daniel.jakob.jackson@gmail.com");
+  navigator.clipboard.writeText("daniel.jakob.jackson@gmail.com");
 
-    let copyEmailPopup = document.getElementById("emailcopy-popup");
-    let copyEmailPopupList = Array.from(copyEmailPopup.classList);
-    if (!copyEmailPopupList.includes("triggered")) {
-        copyEmailPopup.classList.add("triggered");
+  let copyEmailPopup = document.getElementById("emailcopy-popup");
+  let copyEmailPopupList = Array.from(copyEmailPopup.classList);
+  if (!copyEmailPopupList.includes("triggered")) {
+    copyEmailPopup.classList.add("triggered");
 
-        setTimeout(() => {
-            copyEmailPopup.classList.remove("triggered");
-        }, 2000);
-    }
+    setTimeout(() => {
+      copyEmailPopup.classList.remove("triggered");
+    }, 2000);
+  }
 }
 
 function handleNavClick(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    
-    let currInnerHTML = e.target.innerHTML.toLowerCase().replace("•", "").trim();
+  e.preventDefault();
+  e.stopPropagation();
 
-    switch (currInnerHTML) {
-        case "about":
-            document.getElementById("main-page-content-wrapper").classList.remove("work")
-            document.getElementById("main-page-content-wrapper").classList.remove("contact")
-            document.getElementById("main-page-content-wrapper").classList.remove("resume")
-            document.getElementById("main-page-content-wrapper").classList.add("about")
-            document.getElementById("about-link").innerHTML = "<p>About &#x2022</p>"
-            document.getElementById("work-link").innerHTML = "<p>Work</p>"
-            document.getElementById("contact-link").innerHTML = "<p>Contact</p>"
-            break;
-        case "work":
-            document.getElementById("main-page-content-wrapper").classList.remove("about")
-            document.getElementById("main-page-content-wrapper").classList.remove("contact")
-            document.getElementById("main-page-content-wrapper").classList.remove("resume")
-            document.getElementById("main-page-content-wrapper").classList.add("work")
-            document.getElementById("about-link").innerHTML = "<p>About</p>"
-            document.getElementById("work-link").innerHTML = "<p>Work &#x2022</p>"
-            document.getElementById("contact-link").innerHTML = "<p>Contact</p>"
-            break;
-        case "contact":
-            document.getElementById("main-page-content-wrapper").classList.remove("about")
-            document.getElementById("main-page-content-wrapper").classList.remove("work")
-            document.getElementById("main-page-content-wrapper").classList.remove("resume")
-            document.getElementById("main-page-content-wrapper").classList.add("contact")
-            document.getElementById("about-link").innerHTML = "<p>About</p>"
-            document.getElementById("work-link").innerHTML = "<p>Work</p>"
-            document.getElementById("contact-link").innerHTML = "<p>Contact &#x2022</p>"
-            break;
-        default:
-            break;
-    }
+  let currInnerHTML = e.target.innerHTML.toLowerCase().replace("•", "").trim();
+
+  switch (currInnerHTML) {
+    case "about":
+      document
+        .getElementById("main-page-content-wrapper")
+        .classList.remove("experience");
+      document
+        .getElementById("main-page-content-wrapper")
+        .classList.remove("contact");
+      document
+        .getElementById("main-page-content-wrapper")
+        .classList.remove("resume");
+      document
+        .getElementById("main-page-content-wrapper")
+        .classList.add("about");
+      document.getElementById("about-link").innerHTML = "<p>About &#x2022</p>";
+      document.getElementById("experience-link").innerHTML =
+        "<p>Experience</p>";
+      document.getElementById("contact-link").innerHTML = "<p>Contact</p>";
+      break;
+    case "experience":
+      document
+        .getElementById("main-page-content-wrapper")
+        .classList.remove("about");
+      document
+        .getElementById("main-page-content-wrapper")
+        .classList.remove("contact");
+      document
+        .getElementById("main-page-content-wrapper")
+        .classList.remove("resume");
+      document
+        .getElementById("main-page-content-wrapper")
+        .classList.add("experience");
+      document.getElementById("about-link").innerHTML = "<p>About</p>";
+      document.getElementById("experience-link").innerHTML =
+        "<p>Experience &#x2022</p>";
+      document.getElementById("contact-link").innerHTML = "<p>Contact</p>";
+      break;
+    case "contact":
+      document
+        .getElementById("main-page-content-wrapper")
+        .classList.remove("about");
+      document
+        .getElementById("main-page-content-wrapper")
+        .classList.remove("experience");
+      document
+        .getElementById("main-page-content-wrapper")
+        .classList.remove("resume");
+      document
+        .getElementById("main-page-content-wrapper")
+        .classList.add("contact");
+      document.getElementById("about-link").innerHTML = "<p>About</p>";
+      document.getElementById("experience-link").innerHTML =
+        "<p>Experience</p>";
+      document.getElementById("contact-link").innerHTML =
+        "<p>Contact &#x2022</p>";
+      break;
+    default:
+      break;
+  }
 }
--- retrieve.js
+++ retrieve.js
@@ -1,58 +1,64 @@
-
-let data = fetch("../projs.json", {
-    method: "GET",
-    mode: "cors",
+fetch("../projs.json", {
+  method: "GET",
+  mode: "cors",
 })
-.then(response => {
-    return response.json()
-})
-.then(projectData => {
+  .then((response) => {
+    return response.json();
+  })
+  .then((projectData) => {
     arrayOfProjects = projectData;
     let projectsElement = document.getElementById("main-page-projects-wrapper");
     projectData.forEach((proj, idx) => {
-        let projectLink = document.createElement("a");
-        projectLink.innerHTML = "Go to project &rarr;";
-        projectLink.setAttribute("href", proj.ProjectURL);
-        projectLink.setAttribute("rel", "noreferrer");
-        projectLink.setAttribute("target", "_blank");
-        
-        let projectLinkDiv = document.createElement("div");
-        projectLinkDiv.appendChild(projectLink);
+      projectsElement.appendChild(createTile(proj, idx, proj.isProject));
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-        let projectDetails = document.createElement("p");
-        projectDetails.innerHTML = proj.projectDetails;
+function createTile(proj, idx, isProject) {
+  let projectLink = document.createElement("a");
+  projectLink.innerHTML = isProject
+    ? "Go to project &rarr;"
+    : "Go to site &rarr;";
+  projectLink.setAttribute("href", proj.ProjectURL);
+  projectLink.setAttribute("rel", "noreferrer");
+  projectLink.setAttribute("target", "_blank");
 
-        let projectTitle = document.createElement("h4");
-        projectTitle.textContent = proj.projectTitle;
+  let projectLinkDiv = document.createElement("div");
+  projectLinkDiv.appendChild(projectLink);
 
-        let hoverArea = document.createElement("div");
-        hoverArea.appendChild(projectTitle);
-        hoverArea.appendChild(projectDetails);
-        hoverArea.appendChild(projectLinkDiv);
+  let projectDetails = document.createElement("p");
+  projectDetails.innerHTML = proj.projectDetails;
 
-        let projectImg = document.createElement("img");
-        projectImg.setAttribute("src", proj.projectImg);
-        projectImg.setAttribute("alt", proj.projectImgDesc);
-        
-        let projectItem = document.createElement("div");
-        projectItem.addEventListener("touchstart", function(e){
-            e.stopPropagation();
-            let classList = Array.from(projectItem.classList);
-            if (!classList.includes("hover_effect")) {
-                projectItem.classList.add("hover_effect");
-        
-                setTimeout(() => {
-                    projectItem.classList.remove("hover_effect");
-                }, 8000);
-            }
-        });
-        projectItem.setAttribute("class", "projects-item")
-        projectItem.setAttribute("style", `--order: ${idx }`)
-        projectItem.appendChild(projectImg);
-        projectItem.appendChild(hoverArea);
-        projectsElement.appendChild(projectItem);
-    })
-})
-.catch(err => {
-    console.error(err)
-})
+  let projectTitle = document.createElement("h4");
+  projectTitle.textContent = proj.projectTitle;
+
+  let hoverArea = document.createElement("div");
+  hoverArea.appendChild(projectTitle);
+  hoverArea.appendChild(projectDetails);
+  hoverArea.appendChild(projectLinkDiv);
+
+  let projectImg = document.createElement("img");
+  projectImg.setAttribute("src", proj.projectImg);
+  projectImg.setAttribute("alt", proj.projectImgDesc);
+
+  let projectItem = document.createElement("div");
+  projectItem.addEventListener("touchstart", function (e) {
+    e.stopPropagation();
+    let classList = Array.from(projectItem.classList);
+    if (!classList.includes("hover_effect")) {
+      projectItem.classList.add("hover_effect");
+
+      setTimeout(() => {
+        projectItem.classList.remove("hover_effect");
+      }, 8000);
+    }
+  });
+  projectItem.setAttribute("class", "projects-item");
+  projectItem.setAttribute("style", `--order: ${idx}`);
+  projectItem.appendChild(projectImg);
+  projectItem.appendChild(hoverArea);
+
+  return projectItem;
+}
